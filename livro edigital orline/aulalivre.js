91058eb01106837c6ddfe4b8bab9cf43ccdd79cf
tekdodo <164498899+tekdodo@users.noreply.github.com>
2025-04-14 12:00:26 -0300

    Update aulalivre.js

--- livro edigital orline/aulalivre.js
+++ livro edigital orline/aulalivre.js
@@ -1,6 +1,5 @@
-  let livros = require("./livros.json")
-    
-    function mostraLivros(){
+let livros = require("./livros.json")
+function mostraLivros(){
     livros.forEach((Livro) => {
         console.log(Livro.titulo + "_" +
                     Livro.autor + "ano:" +
@@ -8,17 +7,34 @@
                     Livro.páginas + "preço: R$ " +
                     Livro.preco)
                     
-    }) 
+    })
+
+
+}
+function adicionalivro(titulo, autor, editora, ano, sinopse, paginas, genero, preco){
+      livros.push({
+        titulo: titulo,
+        autor: autor,
+        editora: editora,
+        ano: ano,
+        sinopse: sinopse,
+        pagina: paginas,
+        genero: genero,
+        preco: preco,
+        
+      });
+      console.log("Livro adicionado copm sucesso!");
+
 }
 
+adicionalivro("O Pequeno Principe", "Leonardo Davinci", "Tilibra", 2025, "Um príncipe que era pequeno", 1, ["terror", "webtoon", "comédia"], 500.99);
 // JSON = JavaScript Object Notation
 // GERA UM ARQUIVO JSON DOS LIVROS
 function criarArquivo() {
     let livrosTexto = JSON.stringify(livros);
-    const fs = require('fs');
+    const fs = require ('fs');
     fs.writeFileSync("livros.json", livrosTexto);
 }
 
 //criarArquivo()
 mostraLivros();
-
